5db56c590bdc4cb5975759400ddbe25806b67da2
ldl <849781856@qq.com>
2018-11-11 16:04:47 +0800

    1

---
+++ com/nuonuo/test/test1.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+import requests
+
+def searchBaiDu(sk):
+    params = {"wd":sk}
+    resp = requests.get("http://www.baidu.com/s",params=params)
+    if resp.ok:
+        assert sk in resp.text
+        print("SUCCESS")
+    else:
+        print("CHECK NOTHING")
+
+if __name__ == "__main__":
+    searchBaiDu("jmeter")
